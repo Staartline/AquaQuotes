30fe2bad96e6a2af25be9a061177c39cc46ec08f
Staartline <113581885+Staartline@users.noreply.github.com>
2023-10-05 01:04:24 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,103 @@
+
+
+
+// //Array of arrays containing two strings 
+// [["1","2"], ["1","2"]....["n-1","n-2"]]
+// format:["1 -> quote"], ["2 -> Speaker"]
+
+
+
+let quotes = [];
+
+quotes = quoteImport("..\AquaQuotes.txt");
+
+//Array that holds quotes
+//delimiter is "#. "
+//arrays can hold multiple items. 2 strings ??? I might not do this
+//But if the string ends with "" " then it ends the process and looks for the next "*. "
+
+
+//So we wanna read in the file
+//add it to an array
+//
+
+//add a function
+
+function quoteImport(filename){
+
+	//Make an array here with two elements (quote and speaker)
+	//Pop the array onto the end of another array (arraypog2)
+	//return arraypog2
+
+	let QuoteSpeakerArray = [];
+
+	// let file = fetch(filename);
+
+	// let reader = new FileReader(file);
+
+	// let massivestring = reader.readAsText();
+
+	// console.log(massivestring);
+
+	// let parsable = massivestring.split("\n");
+	//contributed by zenonnnn pog
+	fetch(filename).then(result => console.log(result.text()));
+
+	//Import the file
+	//take the text
+	//break it on a newline (or the delimiter of like, #. ) into like a list 
+	//list or something 
+	//loop through the list breaking the quotes into two sections
+	//add those sections into a bigger array with part 1, part 2. 
+
+
+
+	//for loop here that repeats for all the lines in the quotes line
+	//For line in file put it as one string that then gets parsed. So we can 
+	//parse the line on a /n character
+	for (let i = 0; i < parsable.length; i++) {
+		let QuoteSpeaker = [];
+		//parse parsable. 
+		QuoteSpeaker.push();
+		QuoteSpeaker.push();
+		QuoteSpeakerArray.push(QuoteSpeaker);
+	}
+
+	
+
+
+	
+	return
+}
+
+
+
+      function readFile(input) {
+        let file = input.files[0]; 
+        let fileReader = new FileReader(); 
+        fileReader.readAsText(file); 
+        fileReader.onload = function() {
+          alert(fileReader.result);
+        }; 
+        fileReader.onerror = function() {
+          alert(fileReader.error);
+        }; 
+      }
+
+function rngQuote(){
+	const quoteElement = document.getElementById("quoteElement");
+	const speakerElement = document.getElementbyId("speakerElement");
+	
+	//randomint assigns random number to a variable
+	//the variable calls a quote in the quotes array
+
+	//replace html quote with said quote using dom 
+
+	//To replace html quotes its 
+	quoteElement.innerHTML = quote[index[1]];
+	speakerElement.innerHTML = quote[index[2]];
+
+
+
+
+}
