7fb597d3b2a28633eddf50ac2a49304d21ad85b7
Staartline <113581885+Staartline@users.noreply.github.com>
2023-10-05 14:04:02 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,77 +1,37 @@
 
-
+var quotes = [];
 
 // //Array of arrays containing two strings 
 // [["1","2"], ["1","2"]....["n-1","n-2"]]
 // format:["1 -> quote"], ["2 -> Speaker"]
 
+if (quotes == null){
+	declareQuotes();
+}
+rngQuote();
 
 
-let quotes = [];
-quotes = quoteImport("./AquaQuotes.txt");
+function declareQuotes(){
+	quotes = quoteImport("./AquaQuotes.txt");
+}
 
-
-//Array that holds quotes
-//delimiter is "#. "
-//arrays can hold multiple items. 2 strings ??? I might not do this
-//But if the string ends with "" " then it ends the process and looks for the next "*. "
-
-
-//So we wanna read in the file
-//add it to an array
-
-//fetch, async, await 
 async function quoteImport(filename){
 
 	//empty array to hold all arrays of quote, speake
 	let QuoteSpeakerArray = [];
 
 	//contributed by zenonnnn pog
-	let massivestring = await fetch(new Request(filename)).then(await result => result.text());
+	let massivestring = await fetch(new Request(filename)).then(result => result.text());
 
-	let fileLines = massivestring.split("\r\n");
+	let QuoteSpeakerArray = massivestring.split("\r\n");
 
-	// console.log(fileLines.length);
-	
-	//Import the file 
-	//take the text
-	//break it on a newline (or the delimiter of like, #. ) into like a list 
-	//loop through the list breaking the quotes into two sections
-	//add those sections into a bigger array with part 1, part 2. 
-
-
-
-	//for loop here that repeats for all the lines in the quotes line
-	//For line in file put it as one string that then gets parsed. So we can 
-	//parse the line on a /n character
-	for(let i = 0; i < fileLines.length; i++) {
-		let workingString = fileLines[i];
-		let QuoteSpeaker = (workingString.split("\" "));
-		QuoteSpeaker[0] = QuoteSpeaker[0] + "\"";
-		QuoteSpeakerArray.push(QuoteSpeaker);
-	//Make an array here with two elements (quote and speaker)
-	//Pop the array onto the end of another array (arraypog2)
-	//return arraypog2
-	}
-	
 	return QuoteSpeakerArray;
 }
 
-async function rngQuote(){
-	if(quotes == null){
-		quotes = await quoteImport("./AquaQuotes.txt");
-	}
-	
+function rngQuote(){
 	const rngQuote = Math.floor(Math.random() * (quotes.length)) + 1;
-	//randomint assigns random number to a variable
-	//the variable calls a quote in the quotes array
 
-	//replace html quote with said quote using dom 
+	document.getElementById("quoteElement").innerHTML = quotes[rngQuote];
 
-	//To replace html quotes its 
-	document.getElementById("quoteElement").innerHTML = 
-	quotes[rngQuote];
 	console.log(quotes[rngQuote]);
-	document.getElementById("speakerElement").innerHTML = 
-	quotes[rngQuote];
 }
